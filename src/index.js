134ca53031d3fbf9731957e7bef0a264064fac0a
Buse Şentürk <buse.senturk@outlook.com>
2021-06-20 16:37:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let newtask = document.querySelector("#new-task-description").value;
+  let tasks = document.querySelector("#tasks").value;
+let tasks = newtask;
+
+
+
+
+
   // your code here
 });
